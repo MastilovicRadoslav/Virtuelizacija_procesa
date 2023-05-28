6f18e648fbcd39f28eb356b65b3f0714fd7b4983
MastilovicRadoslav <radoslav930@gmail.com>
2023-05-28 16:48:14 +0200

    Server->Program..cs

--- Projekat_Zadatak1/Server/Program.cs
+++ Projekat_Zadatak1/Server/Program.cs
@@ -3,11 +3,17 @@ using System.ServiceModel;
 
 namespace Server
 {
-    public class Program
-    {
-        static void Main()
-        {
-
-        }
-    }
+	public class Program
+	{
+		static void Main()
+		{
+			using (ServiceHost host = new ServiceHost(typeof(Connection)))
+			{
+				host.Open();
+				Console.WriteLine("The service has been successfully started!!!");
+				Console.ReadKey();
+				host.Close();
+			}
+		}
+	}
 }
